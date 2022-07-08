ce6f30a60c162bda863274b811b28bfa13e46f64
Renan Rhaldney <renan.rhaldney@gmail.com>
2022-07-07 21:30:18 -0300

    Corrigido

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -6,16 +6,21 @@ public class Exercicio2 {
         while (contador < N) {
             System.out.print(num1 + " ");
 
+            //Verificar sé o número de entrada faz parte da sequência de Fibonacci
+            if (N == num1){
+                System.out.printf("(%d)\n\nNúmero %d PERTENCE a sequência de Fibonacci!\n", num1, num1);
+                break;
+            }
+            else if (num1 > N) {
+                System.out.printf("\n\nO número %d NÃO pertence a sequência de Fibonacci\n", num1);
+                break;
+            }
+
             int num3 = num2 + num1;
             num1 = num2;
             num2 = num3;
             contador = contador + 1;
 
-            //Verificar sé o número de entrada faz parte da sequência de Fibonacci
-            if (N == num1){
-                System.out.printf("(%d)\nNúmero %d pertence a sequência de Fibonacci!", num1, num1);
-                break;
-            }
         }
     }
     public static void main(String[] args){
